4b442ee58d029ec8e8a57bfb6b16412590295c10
mithun thambawita <mithunthambavita@gmail.com>
2023-04-28 00:14:21 +0530

    add get method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "Simple-demo-project";
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
     }
     @PostMapping("/saveUser")
     public UserDTO saveUser(@RequestBody UserDTO userDTO){
